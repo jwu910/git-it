b64ffadf82781b57f3cf36524c0ba291cb225d48
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-01-08 10:39:21 -0800

    Add GET api call. Need to build auth in to app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,43 @@
-export const start = () => {}
+const axios = require('axios');
+const prompts = require('prompts');
+const program = require('commander');
+const updateNotifier = require('update-notifier');
+const path = require('path');
+
+const pkg = require(path.resolve(__dirname, '../package.json'));
+const notifier = updateNotifier({ pkg });
+
+if (notifier.update) {
+  notifier.notify();
+}
+
+export const start = args => {
+  program
+    .version(pkg.version, '-v, --version')
+    .option('-u, --user <username>', 'Query User')
+    .parse(process.argv);
+
+  const username = program.user
+
+  const options = {
+    method: 'GET',
+    baseURL: 'https://api.github.com',
+    url: `/users/${String(username)}/repos`,
+    headers: {
+      'Accept': 'application/json',
+      'User-Agent': `${username}`
+    }
+  }
+
+  let reposList = [];
+
+  axios(options).then(
+    results => {
+      console.log(results.data);
+    }
+  ).catch(
+    error => {
+      console.error('Error : ', error);
+    }
+  );
+}
