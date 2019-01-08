aa518645c8c775a5dbd1463e784ff5ed7e06afc0
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-01-08 10:38:48 -0800

    Add basic gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,8 @@
-var babel = require('gulp-babel');
-var chalk = require('chalk');
-var gulp = require('gulp');
+var gulp = require("gulp");
+var babel = require("gulp-babel");
 
-gulp.task('build', function() {
-  return gulp
-    .src('src/**/*.js')
+gulp.task("build", function () {
+  return gulp.src("src/app.js")
     .pipe(babel())
-    .on('error', function(error) {
-      process.stderr.write(chalk.red.bold('Build failed.') + '\n');
-      process.stderr.write(error.fileName + '\n');
-      process.stderr.write(error.stack + '\n');
-
-      this.emit('end');
-    })
-    .pipe(gulp.dest('dist'));
-});
-
-gulp.task('watch', function() {
-  gulp.watch('src/**/*.js', ['build']);
+    .pipe(gulp.dest("dist"));
 });
