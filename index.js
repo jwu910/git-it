65335c2c6df73bdf9cb41765514b4aa3586f1d4c
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-02-23 02:05:27 -0800

    Add dotenv add api calls to graphql

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
+require('dotenv').config();
 
 const app = require('./dist/app');
 
 app.start(process.argv.slice(2));
-
---
+++ src/api/getRepos.js
@@ -0,0 +1,31 @@
+import client from './client';
+
+export const getRepos = async username => {
+  try {
+    const res = await client.request(
+      `{
+        user(login: "${username}") {
+          name
+          repositories(first: 100 orderBy:{field: STARGAZERS, direction: DESC}) {
+            edges {
+              node {
+                id
+                name
+                forks {
+                  totalCount
+                }
+                stargazers {
+                  totalCount
+                }
+              }
+            }
+          }
+        }
+      }`,
+    );
+
+    return res;
+  } catch (error) {
+    return 'Error: ', error;
+  }
+};
