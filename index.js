b22f8486fa1e7b38de6a6f53b7997c15d1941310
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-02-25 08:59:51 -0800

    Fail if no env var set

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
 require('dotenv').config();
 
+if (!process.env.GITHUB_TOKEN) {
+  console.error('Environment variable GITHUB_TOKEN is required to be set before Git Started can be run')
+  console.error('Refer to the README or look look at .env.sample for an example')
+  process.exit(1)
+}
+
 const app = require('./dist/app');
 
 app.start(process.argv.slice(2));
