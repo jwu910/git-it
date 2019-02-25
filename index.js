03901e72da4322c899d44e64003d1fd64a125fb9
Joshua Wu <Joshua.wu.0910@gmail.com>
2019-02-25 09:58:31 -0800

    GS-2 SF

--- index.js
+++ index.js
@@ -2,9 +2,13 @@
 require('dotenv').config();
 
 if (!process.env.GITHUB_TOKEN) {
-  console.error('Environment variable GITHUB_TOKEN is required to be set before Git Started can be run')
-  console.error('Refer to the README or look look at .env.sample for an example')
-  process.exit(1)
+  console.error(
+    'Environment variable GITHUB_TOKEN is required to be set before Git Started can be run',
+  );
+  console.error(
+    'Refer to the README or look look at .env.sample for an example',
+  );
+  process.exit(1);
 }
 
 const app = require('./dist/app');
